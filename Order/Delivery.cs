881255f1ded380b4df994a5e7bc899eaa52e9d03
sTorov <sotorov2011@yandex.ru>
2022-06-30 01:35:18 +0300

    Создание файлов Delivery.cs и Order,cs
    Delivery.cs - логика работы различных видов доставок
    Order.cs - логика заказа и содержащейся в нём информации

---
+++ Order/Delivery.cs
@@ -0,0 +1,22 @@
+﻿namespace Delivery
+{
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+}
---
+++ Order/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Order
+{
+    using Delivery;
+
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        // ... Другие поля
+    }
+}
