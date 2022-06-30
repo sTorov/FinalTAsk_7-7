42413dbf0c59fd4e0e4fcc52847bf91dc84c89cc
sTorov <sotorov2011@yandex.ru>
2022-06-30 04:42:15 +0300

    Созданы файлы Person.cs и Company.cs
    Person.cs - содержит логику работы с информацие о курьерах и заказчиках
    Company.cs - содержит информацию о курьерской компании

---
+++ Order/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace Company
+{
+    class Company
+    {
+        protected internal string Address { get; private set; }
+        protected internal string Name { get; private set; }
+        protected internal string HotLinePhone { get; private set; }
+    }
+}
---
+++ Order/Person.cs
@@ -0,0 +1,49 @@
+﻿namespace Delivery
+{
+    using Company;
+
+    abstract class Person
+    {
+        protected string FirstName { get; set; }
+        protected string SecondName { get; set; }
+        protected string LastName { get; set; }
+        protected string Age { get; set; }
+        protected string PhoneNumber { get; set; }
+
+        public abstract void GetInfo();
+    }
+
+    class Client : Person
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine("-------Информация о заказчике-------");
+            Console.WriteLine($"Имя: {FirstName??"Нет данных"}");
+            Console.WriteLine($"Фамилия: {SecondName??"Нет данных"}");
+            Console.WriteLine($"Отчество: {LastName??"Нет данных"}");
+            Console.WriteLine($"Возраст: {Age??"Нет данных"}");
+            Console.WriteLine($"Номер телефона: {PhoneNumber??"Нет данных"}");
+        }
+    }
+    class Staff : Person 
+    { 
+        Company Company = new Company();
+
+        public override void GetInfo()
+        {
+            Console.WriteLine("-------Информация о курьере-------");
+            Console.WriteLine($"Имя: {FirstName ?? "Нет данных"}");
+            Console.WriteLine($"Фамилия: {SecondName ?? "Нет данных"}");
+            Console.WriteLine($"Отчество: {LastName ?? "Нет данных"}");
+            Console.WriteLine($"Возраст: {Age ?? "Нет данных"}");
+            Console.WriteLine($"Номер телефона: {PhoneNumber ?? "Нет данных"}");
+        }
+        public void GetCompanyInfo()
+        {
+            Console.WriteLine("-------Курьерская служба-------");
+            Console.WriteLine($"Название: {Company?.Name ?? "Нет данных"}");
+            Console.WriteLine($"Адрес: {Company?.Address ?? "Нет данных"}");
+            Console.WriteLine($"Телефон горячей линии: {Company?.HotLinePhone ?? "Нет данных"}");
+        }
+    }
+}
