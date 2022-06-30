e8bdeb68988859ddd916b8f2b31b07c80c01eb2f
sTorov <sotorov2011@yandex.ru>
2022-06-30 03:20:27 +0300

    Изменения Order.cs
    Добавлены поля number, description, Delivery, orderReceived
    Добавлены свойства Number, Description, OrderReceived

--- Order/Order.cs
+++ Order/Order.cs
@@ -2,19 +2,40 @@
 {
     using Delivery;
 
-    class Order<TDelivery, TStruct> where TDelivery : Delivery
+    class Order<TDelivery, TNumber> where TDelivery : Delivery
     {
         public TDelivery Delivery;
+        
+        private TNumber number;
+        public TNumber Number
+        {
+            get 
+            { 
+                return number; 
+            }
+        }
+        
+        private string description;
+        public string Description
+        {
+            get 
+            { 
+                return description; 
+            }
+            private set
+            {
+                description = value;
+            }
+        }
 
-        public int Number;
-
-        public string Description;
+        private bool orderReceived;
+        public bool OrderReceived { get; private set; }
 
         public void DisplayAddress()
         {
             Console.WriteLine(Delivery.Address);
         }
-
+        
         // ... Другие поля
     }
 }
