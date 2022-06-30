5484150401a319a3996e3bc8875f970810f97d4e
sTorov <sotorov2011@yandex.ru>
2022-06-30 06:20:44 +0300

    Добавлены файлы Program.cs и Product.cs
    Product.cs - содержит информацию о товарах, предлагаемых интернет-магазином и
    логику работы с товарами
    Program.cs - тестирования функционала

---
+++ Order/Product.cs
@@ -0,0 +1,100 @@
+﻿namespace Product
+{
+    abstract class Product
+    {
+        protected internal string Name { get; internal set; }
+        protected internal double Cost { get; internal set; }
+        protected internal string Model { get; internal set; }
+        protected internal string Description { get; internal set; }
+    }
+
+    class PowerSupply : Product 
+    {
+        protected internal int Power { get; internal set; }
+    }
+    class Processor : Product
+    {
+        protected internal double Frequency { get; internal set; }
+    }
+    class HardDrive : Product
+    {
+        protected internal double Memory { get; internal set; }
+    }
+    class ComputerPart
+    {
+        protected internal PowerSupply PowerSupply { get; internal set; }
+        protected internal HardDrive HardDrive { get; internal set; }
+        protected internal Processor Processor { get; internal set; }
+    }
+    enum MotherBoard
+    {
+        ATX = 0,
+        MiniITX
+    }
+
+    class Dimensions
+    {
+        private int Width;
+        private int Height;
+        private int Length;
+
+        protected internal Dimensions(int Width, int Height, int Length)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            this.Length = Length;
+        }
+    }
+
+    abstract class Corps : Product
+    {
+        protected internal string CorpsMaterial { get; set; }
+
+    }
+
+    class ATX<TCorps> : Corps where TCorps : Corps
+    {
+        protected MotherBoard MotherBoard = MotherBoard.ATX;
+        protected Dimensions Dimensions = new Dimensions(210, 410, 478);
+    }
+
+    class MiniITX<TCorps> : Corps where TCorps : Corps
+    {
+        protected MotherBoard MotherBoard = MotherBoard.MiniITX;
+        protected Dimensions Dimensions = new Dimensions(185, 274, 360);
+    }
+
+    abstract class Computer<TCorps> : Corps where TCorps : Corps, new()
+    {
+        protected TCorps Corps = new TCorps();
+        protected ComputerPart part = new ComputerPart();
+
+        protected internal abstract void ChangeComputerPart<TComputerPart>(TComputerPart newPart) where TComputerPart : ComputerPart;
+
+    }
+
+    class ATXComputer<TComputer> : Computer<ATX<Corps>> 
+    {
+        protected internal override void ChangeComputerPart<TComputerPart>(TComputerPart newPart) 
+        { 
+            part = new ComputerPart
+            {
+                HardDrive = newPart?.HardDrive ?? part.HardDrive,
+                PowerSupply = newPart?.PowerSupply ?? part.PowerSupply,
+                Processor = newPart?.Processor ?? part.Processor
+            };
+        }
+    }
+    class MiniITXComputer<TComputer> : Computer<MiniITX<Corps>>
+    {
+        protected internal override void ChangeComputerPart<TComputerPart>(TComputerPart newPart) 
+        {
+            part = new ComputerPart
+            {
+                HardDrive = newPart?.HardDrive ?? part.HardDrive,
+                PowerSupply = newPart?.PowerSupply ?? part.PowerSupply,
+                Processor = newPart?.Processor ?? part.Processor
+            };
+        }
+    }
+}
---
+++ Order/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Order
+{
+    using Person;
+    using Product;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Staff staff = new Staff();
+            staff.GetInfo();
+            staff.GetCompanyInfo();
+
+            Console.WriteLine();
+
+            ATXComputer<ATX<Corps>> computer = new ATXComputer<ATX<Corps>>();
+            MiniITXComputer<MiniITX<Corps>> computer1 = new MiniITXComputer<MiniITX<Corps>>();
+            ComputerPart newPart = new ComputerPart { 
+                Processor = new Processor{ Frequency = 2.5, Model = "xxx", Cost = 10000, Description = "dsa", Name = "Proc"},
+                PowerSupply = new PowerSupply { Cost = 2500, Description = "dsa", Model = "qwe", Name = "Pow", Power = 25},
+                HardDrive = new HardDrive { Cost = 12345, Name = "Name", Description = "dsaasd", Memory = 34, Model = "Dsa123"}
+            };
+            computer.ChangeComputerPart(newPart);
+            ComputerPart computerPart = new ComputerPart
+            {
+                Processor = new Processor { Frequency = 4 }
+            };
+            computer.ChangeComputerPart(computerPart);
+
+            Console.ReadKey();
+        }
+    }
+}
